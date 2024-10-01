24e54f5ac694c1e57bea339e64cd8102feb0d79f
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:22 +0530

    Student details with header

--- Student_Details.py
+++ Student_Details.py
@@ -1,3 +1,10 @@
+'''
+Author: Adithya_Manoj
+Date: 01/10/2024
+Python program to get the Student Details
+Version: 1.0
+'''
+
 name=input("Enter The Student Name: ")
 roll_no=int(input("Enter The Student's Roll Number: "))
 cgp=float(input("Enter Your CGP: "))
