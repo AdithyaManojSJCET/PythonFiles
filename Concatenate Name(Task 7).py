afc1ea1fb0365fb502e6a8147705f9fe893e317a
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-10-08 14:16:03 +0530

    Add files via upload

---
+++ Concatenate Name(Task 7).py
@@ -0,0 +1,10 @@
+'''
+Author: Adithya Manoj
+Date: 08/10/2024
+Create, concatenate, and print a string and access a sub-string from a given string.
+'''
+
+str1="Adithya"
+str2="Manoj"
+str3=str1+' '+str2
+print(str3)
