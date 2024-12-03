a6839038daa3c3effa50e3ec08e5139959e37099
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2024-12-03 10:33:59 +0530

    Delete Funvtion to find GCD.py

--- Funvtion to find GCD.py
+++
@@ -1,8 +0,0 @@
-def gcd(num1, num2):
-    if num1%num2==0:
-        return num2
-    else:
-        return gcd(num1,num2&num1)
-
-result=gcd(1220,516)
-print(f"The Greatest Common Divisor is",result)
